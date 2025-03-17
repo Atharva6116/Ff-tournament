f3853113f82c302be2b844d7f9c11315775793b0
Atharva6116 <118288100+Atharva6116@users.noreply.github.com>
2025-03-18 00:47:16 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+let users = [
+    { id: "user1", wallet: 100, isBlocked: false },
+    { id: "user2", wallet: 50, isBlocked: true }
+];
+
+let tournaments = [
+    { id: 1, title: "Solo Rush", prize: "₹500", time: "6:00 PM", joined: 3 },
+    { id: 2, title: "Squad War", prize: "₹1000", time: "8:00 PM", joined: 6 }
+];
+
+let registrations = [
+    { tournamentId: 1, userId: "user1" },
+    { tournamentId: 2, userId: "user2" }
+];
